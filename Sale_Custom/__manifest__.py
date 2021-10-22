75d33f9169dc054ba37bbf67e77f8814514d9943
Hieu Nguyen Minh <77373421+MH1910@users.noreply.github.com>
2021-10-22 16:03:58 +0700

    Add files via upload

---
+++ Sale_Custom/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'VSS Style',
+    'version': '1.0',
+    'author': 'VTD Team',
+    'maintainer': 'Lampv8@viettel.com.vn',
+    'website': '',
+
+    'description': """ Modules edit style in odoo""",
+
+    'depends': ['base', 'sale'],
+    'demo': [],
+    'data': [
+        'view/sale_view.xml'],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+}
